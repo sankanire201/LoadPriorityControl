2331edf09921e9d8326bd67d5027b1e6c92d0c0b
sanka liyanage <sanka.liyanage@groupnire.com>
2024-08-16 23:18:51 +0000

    execute_Control_all_Groups is added

--- LPCv1/Controller/EMSControl.py
+++ LPCv1/Controller/EMSControl.py
@@ -7,32 +7,48 @@ logger = logging.getLogger(__name__)
 class EMSControl:
     
     def __init__(self) -> None:
-        self._controlstratagey={}
+        self._controlstratagey=None
         self._devicegroup=None
+        self._cmd={}
         
-    def execute_Strategy(self,message:any)->None:
-        self._controlstratagey[message['controlType']][0].execute(self._devicegroup,message['cmd'])
-        
-    def add_Controller(self,controlstrategy:ControlStrategy) -> None:
-
-            if controlstrategy._controlType  in self._controlstratagey:
-                self._controlstratagey[controlstrategy._controlType].append(controlstrategy)
-            else:
-                self._controlstratagey[controlstrategy._controlType]=[]
-                self._controlstratagey[controlstrategy._controlType].append(controlstrategy)
-
-    def remove_Controller(self,controlstrategy:ControlStrategy)->None:
-        if  self._controlstratagey:
-            try:
-                self._controlstratagey[controlstrategy._controlType].remove(controlstrategy)
-            except Exception as e:
-                logger.error(f"error in remove controller {e}")
+    def execute_Strategy(self)->None:
+        if not self._devicegroup:
+            logger.error(f"The device group is empty")
+            raise KeyError(f"The device group is empty")
         else:
-            pass
+            if not self._controlstratagey:
+                logger.error(f"did you add the control stratagy ?")
+                raise KeyError(f"did you add the control stratagy ?")
+            else:
+                try:
+                    self._controlstratagey.execute(self._devicegroup,self._cmd)
+                except Exception as e:
+                    logger.error(f"error occured {e}")
+                    raise KeyError(f"error occureds {e}")
         
     def set_Group(self,Group: IoTFacade) -> None:
         self._devicegroup=Group
     
-            
+    def set_Controller(self,controller,cmd)->None:
+        self._controlstratagey=controller
+        self._cmd=cmd
     def remove_Group(self,Group: IoTFacade)->None:
         pass
+    
+    
+    # def add_Controller(self,controlstrategy:ControlStrategy) -> None:
+
+    #         if controlstrategy._controlType  in self._controlstratagey:
+    #             self._controlstratagey[controlstrategy._controlType].append(controlstrategy)
+    #         else:
+    #             self._controlstratagey[controlstrategy._controlType]=[]
+    #             self._controlstratagey[controlstrategy._controlType].append(controlstrategy)
+
+    # def remove_Controller(self,controlstrategy:ControlStrategy)->None:
+    #     if  self._controlstratagey:
+    #         try:
+    #             self._controlstratagey[controlstrategy._controlType].remove(controlstrategy)
+    #         except Exception as e:
+    #             logger.error(f"error in remove controller {e}")
+    #     else:
+    #         pass
--- LPCv1/Controller/LoadPriorityControl.py
+++ LPCv1/Controller/LoadPriorityControl.py
@@ -12,4 +12,5 @@ class LoadPriorityControl(ControlStrategy):
         
     def execute(self, group: IoTDeviceGroup, cmd: any) -> None:
         logger.info(f'Running Load Priority controller on the group {id(group)} with the power consumption of the group {group.get_Facade_Consumption()}')
+        for device_id in group._devices: logger.info(f"the device id {device_id} has the priority: {group._devices[device_id]._priority}")
         
